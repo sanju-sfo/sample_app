a675852e4a337d9880c228f8b3219714c7c101f8
sanjsark <sanjsark@Sanjeev-Sarkars-MacBook-Pro.local>
2012-09-19 14:06:55 -0700

    Added test case for Help page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
+    it "Page should have content help" do
       #visit static_pages_index_path
-      visit '/static_pages/home'
-      page.should have_content("SampleApp")
+      visit '/static_pages/help'
+      page.should have_content("help")
       #response.status.should be(200)
     end
-    it "check title" do
-      visit '/static_pages/help'
+    it "Title should be SampleApp" do
+      visit '/static_pages/home'
       page.should have_css("title" , text: "SampleApp")
       end
     end
